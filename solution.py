e0bd57e65b596afa51b20fc616e5351b02562211
EhsanSl <esalimi1997@gmail.com>
2022-11-22 12:31:55 -0500

    update solution.py
    added the wrd_stack , and updated the main, defined find_first_char() and identifier() , working on the logic now..

--- solution.py
+++ solution.py
@@ -16,18 +16,43 @@ def get_input():
         # we add every line of input in an array as a nested lst , and then pop the last element 
         #and store it as the string to look for 
         line.append(row_input.split())
-         
-    print(line)
+    #print(line)
     return line  
 
+#this function will help us find the first character using devide and conqour method 
+# so the first iteration is as fast as possible 
+def find_first_char():
+    return None 
 
+
+def identifier(mtx , wrd , wrd_stack) :
+    
+    #if  (not find_first_char()):
+        #return False
+    #else : 
+    
+    #while True: 
+        # look_left() 
+        #...
+
+    return None
 
 if __name__ == '__main__' : 
 
     # Get the Input, Seperate the Matrix and the KeyWord 
     mtx = get_input() #mtx represents matrix 
-    wrd =  mtx.pop(-1) # wrd represents the string we're searching for
-    print("The Matrix To search in is:", mtx)
-    print("The String To search for is:", wrd)
+    wrd =  mtx.pop(-1) # wrd represents keyword 
+    wrd_stack = list(wrd) #lst of characters of keyword 
+    
+    #print statements  
+    print("The Matrix is:", mtx)
+    print("The KeyWord is:", wrd)
+    print("The KeyWord_Stack is:", wrd_stack)
+
+    
+    #call the identifier function
+    result = identifier()
+    print(result)
+    
 
 
