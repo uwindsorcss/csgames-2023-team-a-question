217c3e5403c6f6e61a7d95fbcc99af8d09490025
EhsanSl <esalimi1997@gmail.com>
2022-11-28 23:15:03 -0500

    Final Ans
    ...

--- solution.py
+++ solution.py
@@ -1,37 +1,7 @@
 
-'''just an idea
-aybe we need 2 stacks so we can keep track of the index of the keyword that has been found 
-and so if we find any char that matches we addd that char to the second stack and remove that char from our main stack, 
-and if main was empty we know that the entire keyword matches 
-and if we get to a point that there is no match we can simply reconstruct the keyword by transfering back the characters from the 
-second stack to the first stack 
-we can also create a function that does this stack transfer for simplicity and لاتی reasons 
-use try / except for exception handling 
-'''
+#import numpy as np 
 
-'''Talk with mohammad
-he said i should be able to get the n*m of the input first 
-then create another 2D array same with size n*m ( same size as original) __
-and the values that it holds are just true and false and they are corresponding to the 
-attribute "visited_before" of each index of the original matrix. 
-this matrix will help me make sure that i dont visit the same character
-
-
-he was supportive of the 2 stack idea 
-but he said after i made it work i could possibly try to apply it using recursion 
-
-also for out of bound : 
-he said the try / except is okay but the regular way of doing this 
-is that first of all o need the size of the original (n*m) and then when ever 
-i wanna move to the right for example (add (0,1)) then i check that future index will be less than n so i wouldnt go out of the lenght 
-if i wanna move to the top for example (add (1,0)) then i check the future index will be less than m .'''
-
-
-#this function will help us find the first character using devide and conqour method 
-# so the first iteration is as fast as possible 
-import numpy as np 
-
-#this function gets the input and store them linearly
+#gets the input and store them linearly
 def get_input():
     line = [] 
     
@@ -50,13 +20,12 @@ def get_input():
     #print(line)
     return line  
 
-#this function returns the deimension of the matrix 
+#returns the deimension of the matrix 
 def get_size(matrix): 
     return len(matrix) , len(matrix[0])  
 
-#this function create a matrix with dimensions of the 
-#original matrix and initializes each index with False
-def create_bool_mtx(n , m): 
+#create a n * m matrix and initializes each index with False
+#def create_bool_mtx(n , m): 
 
     bool_mtx = np.zeros((n ,m),dtype=bool)
     #print(bool_mtx)
@@ -67,27 +36,59 @@ def create_bool_mtx(n , m):
 # checks to see if the board and input are valid  to pass to scavenger 
 # if everything is good , calls scavenger for every index of the matrix 
 def identifier (mtx, wrd) :
+    
+    n , m = get_size(mtx)
+    if ( mtx == None or wrd == None or wrd.strip() == "" or n <= 0 or m <= 0   ) : 
+        return False #returns false if the mtx or wrd were invalid 
+    
+    #create the bool_array for to keep track of visited nodes 
+    #bool_mtx = create_bool_mtx(n,m)
 
 
-    return None
+    # go through each index in the mtx and see if the wrd wrd 
+    #is constructable from its neighbour indexes 
+    for i in range(0,n) : 
+        for j in range (0,m) : 
+            if Scavenger(mtx, wrd , i , j , 0) : # 0  is passed as the defaukt counter, it will increment in 
+                return True                                 # scavenger if the characters match  
+            
+    # if the scavenger didnt find the match return False 
+    return False 
 
-
-#this function takes 4 variables: 1.Matrix , 2.Boolean_matrix , 3. keyWord, 4. mtx dimensions 
 # To be implemented .. 
-def Scavenger(mtx, bool_mtx, wrd, matched) :
+# n & m are i & j , count is number of matched characters  
+def Scavenger(mtx, wrd, i , j , count) :
     
     #wrd_stack = list(wrd) #lst of characters of keyword
     #print("The KeyWord_Stack is:", wrd_stack)
-    
-    #if  (not find_first_char()):
-        #return False
-    #else : 
-    
-        #while True: 
-        # look_left() 
-        #...
 
-    return None
+    #n is the lenght and m is the height of mtx 
+    n , m = get_size(mtx) 
+
+    #in case we found all the chars 
+    if (len(wrd) == count ): 
+        return True 
+    
+    # return false if :
+    #   charcters dont match 
+    #   i is out of bound or j is out of bound   
+    if  i<0 or n <= i or j<0 or  m <= j or  mtx[i][j] != wrd[count]:
+        #bool_mtx = False  # reseting the value of boolean matrix 
+        return False 
+
+    # this is the case were the characters match and i and j are still in bound 
+    #bool_mtx[i][j] = True  
+    
+    
+    mtx[i][j] = '-' # changing the char to avoid recounting the same index 
+    # using recursion , we go right, left  , up and down respectively from each index here  
+    result =  Scavenger( mtx, wrd, i + 1 , j , count + 1 ) or  \
+    Scavenger( mtx, wrd, i - 1 , j , count + 1 ) or  \
+    Scavenger( mtx, wrd, i , j + 1 , count + 1) or  \
+    Scavenger( mtx, wrd, i , j -1 , count + 1)  
+    mtx[i][j] = wrd[count] # switching back to the original character to keep the originaol mtx
+    return result 
+
 
 
 
@@ -97,7 +98,8 @@ if __name__ == '__main__' :
     print("  ================ WELCOME ================")
     print(f"- Enter each row of matrix and then press 'Enter', \n"
     "- When you are done inputting the matrix , press 'Enter' once more.\n"
-    "- Then you can input the keyword that you'd like to search for!\n Go! === > ")
+    "- Then you can input the keyword that you'd like to search for!\n" 
+    "  =========================================> ")
 
 
     # Get the Input, Seperate the Matrix and the KeyWord 
@@ -106,22 +108,19 @@ if __name__ == '__main__' :
 
     # we get the n, m to create the boolean function
     n , m  = get_size(mtx) #an array holding the dimensions of mtx
-    bool_mtx = create_bool_mtx(n,m)
-
-
-
+    
     #print statements  
     print("  ============== PROCCESSING ==============")
-    print("The Matrix is:", mtx)
-    print("The KeyWord is:", wrd)
-    print(f"Dimentions n = {n}, m = {m}")
-
+    print("- The Matrix is:", mtx)
+    print("- The KeyWord is:", wrd)
+    print(f"- Dimentions n = {n}, m = {m}")
+    result = identifier(mtx, wrd )
+    print(f"- result: {result}")
+    print("  =========================================")
     
     #Scavenger() returns True or False 
     # depending of the presence of the keyword in mtx   
     #result = Scavenger()
-    
-
     exit 
     
 
