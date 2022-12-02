4eb84577a7755829640bba946e49150131c9deac
Brady-Malott <brady.malott@gmail.com>
2022-12-02 03:03:02 -0500

    Created a board class and some boilerplate

---
+++ main.py
@@ -0,0 +1,15 @@
+class Board:
+
+    def __init__(self, board):
+        self._board = board
+        self._stack = []
+
+    def can_construct_word(self, word):
+        pass
+
+    def dfs_construct_word(self, word, r, c):
+        pass
+
+
+if __name__ == '__main__':
+    pass
