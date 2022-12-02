92d2fba2ca1f7a901d70fc331ceb6e49cdf17457
Brady-Malott <brady.malott@gmail.com>
2022-12-02 03:25:33 -0500

    Implemented reading input

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ class Board:
     def dfs_construct_word(self, word, r, c):
         pass
 
+def read_input():
+    with open('input.txt') as f:
+        lines = f.readlines()
+        word = lines.pop()
+        grid = [line.strip().split(' ') for line in lines]
+        return grid, word
 
 if __name__ == '__main__':
-    pass
+    (grid, word) = read_input()
+    print(grid)
+    print(word)
+    board = Board(grid)
+    print("Attempting to construct " + word + ": " + board.can_construct_word(word))
