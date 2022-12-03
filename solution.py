1963522a36b2867fbfc25a429ecd8797be823df9
Derek Thibert <56123156+thibert-derek@users.noreply.github.com>
2022-12-02 20:19:27 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,23 +1,32 @@
-class Solution(object):
-   def exist(self, board, word):
-      n = len(board)
-      m = len(board[0])
-      for i in range(n):
-         for j in range(m):
-            if word[0] == board[i][j]:
-               if self.find(board,word,i,j):
-                  return True
-      return False
-   def find(self, board,word,row,col,i=0):
-      if i== len(word):
-         return True
-      if row>= len(board) or row <0 or col >=len(board[0]) or col<0 or word[i]!=board[row][col]:
-         return False
-      board[row][col] = '*'
-      res = self.find(board,word,row+1,col,i+1) or self.find(board,word,row-1,col,i+1) or self.find(board,word,row,col+1,i+1) or self.find(board,word,row,col-1,i+1)
-      board[row][col] = word[i]
-      return res
-      
+from typing import List
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        ROWS, COLS = len(board), len(board[0])
+        path = set()
+
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            if (r < 0 or c < 0 or
+                r >= ROWS or c >= COLS or
+                word[i] != board[r][c] or
+                (r, c) in path):
+                return False
+            
+            path.add((r, c))
+            res = (dfs(r + 1, c, i + 1) or
+                   dfs(r - 1, c, i + 1) or
+                   dfs(r, c + 1, i + 1) or
+                   dfs(r, c - 1, i + 1))
+            path.remove((r, c))
+            return res
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r, c, 0): return True
+        return False
+
 ob1 = Solution()
 print(ob1.exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]],"SEE"))
 ob2 = Solution()
